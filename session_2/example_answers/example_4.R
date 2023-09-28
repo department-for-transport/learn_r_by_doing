b29105d7f47a97d6ce5777cc9db61d9ec0ded387
Francesca Bryden <fbryden@gcp-rs-aviation.c.dft-ls-datacentre-prod.internal>
2023-09-28 13:38:41 +0100

    Updated CSV location

--- session_2/example_answers/example_4.R
+++ session_2/example_answers/example_4.R
@@ -1,16 +1,16 @@
-#load the libraries 
-
-library(dplyr)
-library (data.table)
-
-#go out an get the data
-#list of columns to include in file
-includecols -> ("country_region","date,retail_and_recreation_percent_change_from_baseline","grocery_and_pharmacy_percent_change_from_baseline","parks_percent_change_from_baseline","transit_stations_percent_change_from_baseline","workplaces_percent_change_from_baseline","residential_percent_change_from_baseline")
-#list of cols to use in the filter for inclusion
-filtercols -> ("country_region","sub_region_1","date")
-mydata -> data.table::fread(file="https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv", %%
-                            select=includecols, %%
-                              )
-
-
-=== the end ===
+# #load the libraries 
+# 
+# library(dplyr)
+# library (data.table)
+# 
+# #go out an get the data
+# #list of columns to include in file
+# includecols -> ("country_region","date,retail_and_recreation_percent_change_from_baseline","grocery_and_pharmacy_percent_change_from_baseline","parks_percent_change_from_baseline","transit_stations_percent_change_from_baseline","workplaces_percent_change_from_baseline","residential_percent_change_from_baseline")
+# #list of cols to use in the filter for inclusion
+# filtercols -> ("country_region","sub_region_1","date")
+# mydata -> data.table::fread(file="https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv", %%
+#                             select=includecols, %%
+#                               )
+# 
+# 
+# === the end ===
